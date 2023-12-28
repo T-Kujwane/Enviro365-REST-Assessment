2332620da13ec71edec0a0daf647ae68899ba5c0
Thato Keith Kujwane <Thato Keith Kujwane@Lenovo>
2023-12-28 06:20:09 +0200

    Created home page in resources/static

---
+++ src/main/java/com/enviro/assessment/grad001/thatokujwane/Enviro365InvestmentsManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.thatokujwane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365InvestmentsManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365InvestmentsManagerAppApplication.class, args);
+	}
+
+}
